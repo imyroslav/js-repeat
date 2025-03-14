b448e9966edd99bb3205b71f0297cd5eafe60938
imyroslav <imyroslav@ukr.net>
2025-03-14 22:05:21 +0100

    update-14.03.25

---
+++ module1.js
@@ -0,0 +1,42 @@
+"use strict";
+
+console.log("2" > "127");
+
+
+
+console.log("Myro".charCodeAt(0));
+
+
+console.log(null == undefined);
+
+console.log(5 >= 4); 
+
+
+function displayMsg() {
+    console.log("Hello World!!?!");
+}
+
+
+displayMsg()
+
+displayMsg()
+
+displayMsg()
+
+
+function greeting(username) {
+    console.log(`Hello, ${username}`)
+}
+
+greeting("Dima")
+greeting()
+
+
+
+
+
+
+
+
+
+
